b0d2d1a650f8bb94f6aceecb877a5e2533aa200e
AAJCrook <113472402+AAJCrook@users.noreply.github.com>
2022-12-02 15:59:05 -0800

    wrote console-ran prototype

--- Budget/Program.cs
+++ Budget/Program.cs
@@ -1,4 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // used to store both
+        Dictionary<string, decimal> inputs = new Dictionary<string, decimal>();
 
 
+
+        Console.WriteLine("Hello! The following application allows you to see your monthly balance");
+
+
+        // take in user input into the cashInput  
+        while (true)
+        {
+            Console.WriteLine("Input the name of your income/expense (ie rent, work pay, insurance, tax credit");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Now please enter the value of your income/expense (ie -600, 423.12");
+
+            decimal value = decimal.Parse(Console.ReadLine());
+
+            inputs.Add(name, value);
+
+            Console.WriteLine("If you would like to continue adding, enter y");
+            if (!(string.Equals(Console.ReadLine(), "y")))
+            {
+                break;
+            }
+        }
+
+        // rudimentary printout of dictionary
+        foreach (KeyValuePair<string, decimal> kvp in inputs)
+        {
+            Console.WriteLine($"Name: {kvp.Key}, Value: {kvp.Value}");
+        }
+    }
+}
